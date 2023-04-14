9ad0a2cfea97b5930aa604d545775d11549f4dea
Siddharth P Bharadwaj <siddharthpb2007@gmail.com>
2023-04-14 14:29:08 +0530

    Initial Commit

---
+++ VoiceLink Watch App/VoiceLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceLinkApp.swift
+//  VoiceLink Watch App
+//
+//  Created by Praveen C Ramamurthy on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct VoiceLink_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceLink/VoiceLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceLinkApp.swift
+//  VoiceLink
+//
+//  Created by Praveen C Ramamurthy on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct VoiceLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
